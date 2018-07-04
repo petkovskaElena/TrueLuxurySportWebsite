8d742f6ed710cd2a62506875675a2c6c51f43c27
Elena Petkovska <elenap@sopro.io>
2018-07-04 11:05:25 +0200

    gulpfile.js and setup changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+, wait = require('gulp-wait');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var image = require('gulp-image');
  
-gulp.task('hello', function() {
-    console.log('Hello World');
-  });
-
   gulp.task('sass', function(){
     // return gulp.src('src/scss/**/*.scss')
        return gulp.src('src/scss/styles.scss')
+      .pipe(wait(500))
       .pipe(sass())
       .pipe(gulp.dest('dist/css'))
   });
@@ -21,6 +20,12 @@ gulp.task('hello', function() {
     })
   });
 
-  gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.task('image', function () {
+    gulp.src('src/images/*')
+      .pipe(image())
+      .pipe(gulp.dest('dist/images'));
+  });
+
+  gulp.task('watch', ['browserSync', 'sass', 'image'], function (){
     gulp.watch('src/scss/**/*.scss', ['sass']); 
   });
