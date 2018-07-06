aae6e6dcbb7da1b73a85cc9b6ff3c0f2252f42e8
Elena Petkovska <elenap@sopro.io>
2018-07-06 09:58:57 +0200

    Initial layout for navbar

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+
+$( document ).ready(function() {
+
+    // Shrinks the navbar when the page is scrolled
+ $(window).scroll(function() {
+    if ($("#mainMenu").offset().top > 100) {
+        $("#mainMenu").addClass("navbar-shrink");
+    } else {
+        $("#mainMenu").removeClass("navbar-shrink");
+    }
+});
+});
