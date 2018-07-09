79c3fff39529621249d1cdd0794ba9bf29bb81f6
Elena Petkovska <elenap@sopro.io>
2018-07-09 09:42:13 +0200

    Design changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,17 @@
-
-$( document ).ready(function() {
+$(document).ready(function () {
 
     // Shrinks the navbar when the page is scrolled
- $(window).scroll(function() {
-    if ($("#mainMenu").offset().top > 100) {
-        $("#mainMenu").addClass("navbar-shrink");
-    } else {
-        $("#mainMenu").removeClass("navbar-shrink");
-    }
-});
+    $(window).scroll(function () {
+        if ($("#mainMenu").offset().top > 100) {
+            $("#mainMenu").addClass("navbar-shrink");
+        } else {
+            $("#mainMenu").removeClass("navbar-shrink");
+        }
+    });
+
+    $("#test").on('click', function () {
+        $("#mainMenu").css("background-color", "rgba(black, .6);");
+    });
+
 
 });
